d095167e7eb175980ad1f79cdd00f6a9c240cf66
Jeremy Nicklas <jnicklas@osc.edu>
2015-12-29 09:41:14 -0500

    added namespaced scaffold

---
+++ lib/generators/osc_machete_rails/scaffold_generator.rb
@@ -0,0 +1,49 @@
+require 'rails/generators/rails/scaffold/scaffold_generator'
+
+class OscMacheteRails::ScaffoldGenerator < Rails::Generators::ScaffoldGenerator
+  attr_reader :orig_args
+
+  def initialize(args, *options)
+    @orig_args = args
+    super
+  end
+
+  # override ModelGenerator
+  remove_hook_for :orm
+
+  # hook for workflow model/migration
+  hook_for :workflow_model, type: :boolean do |model|
+    invoke model, orig_args + add_jobs
+  end
+
+  # hook for job model/migration
+  hook_for :job_model, type: :boolean do |model|
+    new_args = orig_args.dup
+    new_args[0] = jobs_table_name
+    new_args << "#{singular_table_name}:references"
+    invoke model, new_args
+  end
+
+  # override hook for adding config/routes
+  hook_for :resource_route, required: true
+
+  # hook for workflow batch script template
+  hook_for :workflow_template, type: :boolean
+
+  # override ScaffoldGenerator
+  hook_for :scaffold_controller, required: true do |controller|
+    invoke controller, orig_args + add_jobs
+  end
+
+  # remove scaffold_stylesheet
+  remove_hook_for :stylesheet_engine
+
+  private
+    def add_jobs
+      ["#{jobs_table_name}:jobs"]
+    end
+
+    def jobs_table_name
+      "#{singular_table_name}_job"
+    end
+end
--- lib/osc_machete_rails/engine.rb
+++ lib/osc_machete_rails/engine.rb
@@ -1,5 +1,12 @@
 module OscMacheteRails
   class Engine < ::Rails::Engine
     isolate_namespace OscMacheteRails
+
+    config.app_generators do |g|
+      g.workflow_model      true
+      g.job_model           true
+      g.workflow_template   true
+      g.jbuilder            false
+    end
   end
 end
