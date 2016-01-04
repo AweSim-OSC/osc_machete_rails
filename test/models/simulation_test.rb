897c035e75db4ee632412530b2b0795055b41018
Eric Franz <efranz@osc.edu>
2016-01-04 13:25:32 -0500

    tests can run against dummy app now

--- test/models/simulation_test.rb
+++ test/models/simulation_test.rb
@@ -1,15 +1,8 @@
 require 'test_helper'
 
 class SimulationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "create sim" do
-    #FIXME: FAIL
-    #ActiveRecord::StatementInvalid: Could not find table 'simulations'
-    #
-    # @sim = Simulation.create(name: "One", pressure: 200)
-
-    # assert_equal Simulation.first.pressure, 200
+    @sim = Simulation.create(name: "One", pressure: 200)
+    assert_equal Simulation.first.pressure, 200
   end
 end
