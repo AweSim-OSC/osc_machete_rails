c055f1329add8608efca461aef8cab978a107e9e
Eric Franz <efranz@osc.edu>
2016-01-12 18:49:00 -0500

    enable dev to disable adding the before_actions

--- lib/osc_machete_rails/engine.rb
+++ lib/osc_machete_rails/engine.rb
@@ -8,11 +8,17 @@ module OscMacheteRails
       g.workflow_template   true
     end
 
+    config.before_initialize do
+      OscMacheteRails.update_status_of_all_active_jobs_on_each_request = true
+    end
+
     config.after_initialize do
-      # set before action on both Engine controllers and main App controllers
-      # to update the status of all the active jobs
-      ::ApplicationController.before_action -> { OSC::Machete::SimpleJob::Statusable.update_status_of_all_active_jobs }
-      ApplicationController.before_action -> { OSC::Machete::SimpleJob::Statusable.update_status_of_all_active_jobs }
+      if OscMacheteRails.update_status_of_all_active_jobs_on_each_request
+        # set before action on both Engine controllers and main App controllers
+        # to update the status of all the active jobs
+        ::ApplicationController.before_action -> { OSC::Machete::SimpleJob::Statusable.update_status_of_all_active_jobs }
+        ApplicationController.before_action -> { OSC::Machete::SimpleJob::Statusable.update_status_of_all_active_jobs }
+      end
     end
   end
 end
