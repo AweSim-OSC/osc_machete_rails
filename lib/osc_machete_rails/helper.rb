dec3cab949805abb437406af64376de21c89d611
Eric Franz <efranz@osc.edu>
2016-01-19 11:56:17 -0500

    update status label helper to use passed

--- lib/osc_machete_rails/helper.rb
+++ lib/osc_machete_rails/helper.rb
@@ -1,19 +1,21 @@
 module OscMacheteRails
   module Helper
-    def status_label(job)
+    def job_status_label(job)
       job ||= OpenStruct.new status: OSC::Machete::Status.not_submitted
+      text = job.status.inspect
 
       label_class = 'label-default'
       if job.failed?
         label_class = 'label-danger'
-      elsif job.completed?
+      elsif job.passed?
         label_class = 'label-success'
+        text = "Completed"
       elsif job.active?
         label_class = 'label-primary'
       end
 
       content_tag :span, class: %I(label #{label_class}) do
-        job.status.inspect
+        text
       end
     end
   end
