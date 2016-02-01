0b42559ba8ea69e8558133ee48072fdad2ba3839
Eric Franz <efranz@osc.edu>
2016-02-01 14:46:49 -0500

    remove submittable

--- lib/osc_machete_rails.rb
+++ lib/osc_machete_rails.rb
@@ -1,6 +1,5 @@
 require "osc/machete"
 require "osc_machete_rails/engine"
-require "osc_machete_rails/submittable"
 require "osc_machete_rails/statusable"
 require "osc_machete_rails/workflow"
 require "osc_machete_rails/helper"
@@ -8,22 +7,6 @@ require "osc_machete_rails/helper"
 module OscMacheteRails
   mattr_accessor :update_status_of_all_active_jobs_on_each_request
 
-  # Include the Submittable and Statusable modules by default when you include
-  # this module
-  #
-  # FIXME: can we delete this? If this is not needed for backwards
-  # compatibility...
-  #
-  # @param [Object] obj The base object.
-  def self.included(obj)
-    #HACK: we bypass the private visiblity of Module#include
-    # for Ruby 2.0.0; in Ruby 2.1.0 Module#include is public
-    # so this should be safe
-    obj.send :include, OscMacheteRails::Submittable
-    obj.send :include, OscMacheteRails::Statusable
-  end
-
-  # make OSC::Machete::SimpleJob an alias of this module for backwards
-  # compatibility
+  # make OSC::Machete::SimpleJob an alias of this module for backwards compatibility
   OSC::Machete::SimpleJob = self
 end
